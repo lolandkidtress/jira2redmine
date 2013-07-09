28064b47c2b4de89a5002bcd2c279dd53abaa09e
lolandkidtress@gmail.com <lolandkidtress@gmail.com>
2013-07-09 17:09:10 +0800

    add a undo sql

---
+++ other/undo/undo.sql
@@ -0,0 +1,12 @@
+# project
+delete from bitnami_redmine.projects;
+delete from bitnami_redmine.projects_trackers;
+delete from bitnami_redmine.enabled_modules;
+delete from bitnami_redmine.wikis;
+# issue
+delete from bitnami_redmine.issues;
+# custom field
+delete from  bitnami_redmine.custom_values;
+delete from bitnami_redmine.custom_fields;
+delete from bitnami_redmine.custom_fields_trackers;
+delete from bitnami_redmine.custom_fields_projects;
